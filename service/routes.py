b7fa9254546ea2851619f0428e5f133d004ff3c0
Ulises Buendia <ulises.buendia@ibm.com>
2024-09-25 00:47:55 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 # ... place you code here to LIST accounts ...
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
